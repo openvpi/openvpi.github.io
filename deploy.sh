5ac3dce712b8e7b8e140e91acc6ae1c5cddc8eda
yqzhishen <yangqian_1015@icloud.com>
2022-05-06 21:23:47 +0800

    放弃缓存镜像

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@ set -e
 
 pwd
 remote=$(git config remote.origin.url)
-echo "remote is : "$remote
+echo "Remote: "$remote
 
 tmp=gh-pages-branch
 mkdir "${tmp}"
 cd "${tmp}"
 
-echo "configuring git"
+echo "Configuring git"
 sudo chmod 755 /home/circleci/.config
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
-echo "switching branch"
+echo "Switching branch"
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1; then
     git checkout gh-pages
     git rm -rf .
@@ -25,14 +25,14 @@ else
     git checkout --orphan gh-pages
 fi
 
-echo "copying files"
+echo "Copying files"
 cp -a "../${GB_SOURCE}/." .
 
-echo "add, commit & push"
+echo "Add, commit & push"
 git add -A
 git commit --allow-empty -m "Deploy to GitHub Pages by CircleCI"
 git push --force origin gh-pages
 
-echo "cleaning files"
+echo "Cleaning files"
 cd ..
 rm -rf "${tmp}"
